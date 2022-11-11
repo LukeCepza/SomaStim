df8aa2ed9bbc27763c2e7db9adca1dd4e39f0715
DCN-LPTP-11\KevinCepeda <lkcepza@gmail.com>
2022-11-11 14:00:18 -0600

    Ya funcional

--- openvibe/Vibration_20st.lua
+++ openvibe/Vibration_20st.lua
@@ -11,6 +11,8 @@ local stim_rest_length 	= 8 				--duration of stimulation (3) and resting (5)
 local st 		= 20				--number of stimuli
 local stop_vib 		= 33042
 local t_lag			= 20
+local valid        = 33041                --flag to start the validation
+
 -- this function is called when the box is uninitialized
 function uninitialize(box)
 	io.write("uninitialize has been called\n")
@@ -18,6 +20,9 @@ end
 
 function process(box)
 	io.write("process has been called\n")
+	box:send_stimulation(1, valid, 5, 0)        --start validation process   
+	box:log("Info", string.format('%i at %i', valid, 5))
+         
 		n=3
 		for i=1, st, 1 do
 		box:send_stimulation(1, vibracion[n], t_lag + (i-1)*stim_rest_length, 0)		--Start stimuli
