bdc3ee90fbbdd110af5ef4e4e4a8d653f957b1c7
Erick Alejandro <erickalejandro1211@gmail.com>
2022-11-11 21:25:41 -0600

    Variables
    Se agregaron variables entre otras funciones que faltaban para probar para el de caricia.
    No funcionó. Seguir probando.

--- openvibe/Caricia_20st.lua
+++ openvibe/Caricia_20st.lua
@@ -2,24 +2,25 @@
 function initialize(box)
 	io.write("initialize has been called\n");
 	dofile(box:get_config("${Path_Data}") .. "/plugins/stimulation/lua-stimulator-stim-codes.lua")
-	box:log("Info", string.format('begin'))
+	--box:log("Info", string.format('begin'))
 end
 
-local stim_rest_length = 31 --duration of resting + stimulation
+local stim_rest_length = 11 --duration of resting + stimulation
 local tense_length = stim_rest_length -6
 local stim_length = stim_rest_length	--duration of stimulation
 
 local caricia 		= {33024, 33025, 33026, 33027}
 local caricia_tense 	= {33036, 33037, 33038, 33039}
 --local stop_air 		=  33043
-t_lag = 20
+local st  = 20	-- estimulos
+local t_lag = 20
 
 --es el numero de veces que se ha hecho el threshold.
 -- this function is called when the box is uninitialized
 function uninitialize(box)
 	io.write("uninitialize has been calle d\n")
-	box:log("Info", string.format('finished at %i',box:get_current_time()))
-	box:send_stimulation(1,32770,box:get_current_time()+0.1,0)
+	--box:log("Info", string.format('finished at %i',box:get_current_time()))
+	--box:send_stimulation(1,32770,box:get_current_time()+0.1,0)
 end
 
 function process(box)
