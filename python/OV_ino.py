d513e652f6ca59ed050081bdbc369722d1b311ba
DESKTOP-H6AISRD\Kevin <lkcepza@gmail.com>
2023-02-24 00:11:42 -0600

    Version que no necesita LSL; Hace falta probarlo

---
+++ python/OV_ino.py
@@ -0,0 +1,32 @@
+import sys; sys.path.append('./pylsl')
+# from pylsl import StreamInlet, resolve_stream, StreamInfo, StreamOutlet
+import serial
+
+class MyOVBox(OVBox):
+   def __init__(self):
+      OVBox.__init__(self)
+
+   def initialize(self):
+      self.arduino=serial.Serial('COM15',baudrate=500000)
+      return
+
+   def process(self):
+      for chunkIndex in range( len(self.input[0]) ):
+        chunk = self.input[0].pop()
+        if(type(chunk) == OVStimulationSet):
+            # We move through all the stimulation received in the StimulationSet and
+            # we print their date and identifier
+            for stimIdx in range(len(chunk)):
+                stim=chunk.pop()
+                self.arduino.write(str(stim.identifier).encode('utf-8'))
+                print ('At time ', stim.date, ' received stim ', stim.identifier)
+        else:
+                print ('Received chunk of type ', type(chunk), " looking for StimulationSet")
+
+      return
+
+   def uninitialize(self):
+      self.arduino.close()
+      return
+
+box = MyOVBox()
